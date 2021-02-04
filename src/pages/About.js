6f61e73a78e94dd6fd07131d3e57116437f3aad8
Priyank Patel <noreplyonlyspam@gmail.com>
2021-02-04 08:40:19 -0500

    fixed unused var

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Title from "../components/Title"
 import ImageSection from '../components/ImageSection';
-import SkillsSection from '../components/SkillsSection';
 
 import "../styles/about.css"
 import ServicesSection from './../components/ServicesSection';
