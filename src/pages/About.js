688a9e6f6fa5a51040d9f08f2b6809eed391078f
Priyank Patel <noreplyonlyspam@gmail.com>
2021-02-03 19:45:29 -0500

    Organizing content

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,6 +11,18 @@ function About() {
         <div className="AboutPage">
             <Title title={'About Me'} span={'About Me'} />
             <ImageSection />
+            {/* <Title title={'My Skills'} span={'My Skills'} />
+            <div className="skillsContainer">
+                <SkillsSection skill={'Javascript'} progress={'60%'} width={'60%'} />
+                <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
+                <SkillsSection skill={'React Js'} progress={'70%'} width={'70%'} />
+                <SkillsSection skill={'Node Js'} progress={'80%'} width={'80%'} />
+                <SkillsSection skill={'Python'} progress={'90%'} width={'90%'} />
+                <SkillsSection skill={'Lua'} progress={'40%'} width={'40%'} />
+                <SkillsSection skill={'Java'} progress={'80%'} width={'80%'} />
+                <SkillsSection skill={'Web Design'} progress={'25%'} width={'25%'} />
+                <SkillsSection skill={'UI/Ux Design'} progress={'76%'} width={'76%'} />
+            </div> */}
 
             <Title title={'Services'} span={'Services'} />
             <div className="servives-container">
