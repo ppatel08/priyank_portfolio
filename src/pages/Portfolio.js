99d9a7802b09e8bab2e8b0bcdae60892ffb8e064
Priyank Patel <priyank.patel@thephysicianalliance.org>
2021-03-01 11:57:04 -0500

    card ui Done

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Title from "../components/Title"
-import PortfolioSection from './../components/PortfolioSection';
 import sanityClient from './../client';
 import "../styles/portfolio.css"
+import { Link } from 'react-router-dom';
 
 
 
@@ -26,47 +26,18 @@ function Portfolio() {
             .catch(console.error)
     }, [])
     return (
-        // < div >
-        //     <Title title={'Portfolio'} span={'Portfolio'} />
-        //     <div className="servives-container">
-        //         {/* Map after this  */}
-        //         {postData && postData.map((post, index) => {
-        //             console.log(post)
-        //             return (
-        //                 <Link className="test" to={"/portfolio/" + post.slug.current} key={post.slug.current}>
-        //                     <div className="ServicesSection"  >
-        //                         <div className="service">
-        //                             <div className="service-content">
-
-
-        //                                 <h5 className="s-title">{post.title}</h5>
-        //                                 <p className="s-text">
-        //                                     Some Text
-        //                     </p>
-        //                             </div>
-        //                         </div>
-        //                     </div>
-        //                 </Link>
-        //             )
-        //         })}
-        //         {/* End of Mapping */}
-        //     </div>
-        // </ div >
         <>
             <Title title={'Portfolio'} span={'Portfolio'} />
             <div className="portfolio">
                 {postData && postData.map((post, index) => {
                     return (
-                        <>
-
-                            <div className="service">
-                                <span className="portfoliolable">{post.title}</span>
-                                <h3 className="portfolioHeaderTitle">Dental Mobile LLC</h3>
-                                <p className="portfolioDescription">Some kind of wording that will make sense</p>
-                                <button className="btn">Learn More</button>
-                            </div>
-
-                        </>
+                        <div className="service">
+                            <span className="portfoliolable">{post.title}</span>
+                            <h3 className="portfolioHeaderTitle">Dental Mobile LLC</h3>
+                            <p className="portfolioDescription">Some kind of wording that will make sense</p>
+                            <Link to={"/portfolio/" + post.slug.current} key={post.slug.current}> <button className="btn">
+                                Learn More</button></Link>
+                        </div>
                     )
                 })}
             </div>
