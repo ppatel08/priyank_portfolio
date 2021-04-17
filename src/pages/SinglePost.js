c1f849718a0e033820c80f860e090c8db2b9e4a4
Priyank Patel <priyank.patel@thephysicianalliance.org>
2021-04-17 08:40:02 -0400

    Sanity Image Url Npm package

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import sanityClient from './../client';
 
 function SinglePost() {
+
+    const [singlePost, setsinglePost] = useState(null);
+    const { slug } = useParams();
+
     return (
         <div>
             Single Post
